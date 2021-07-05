1d4d75b8b5ccbcd0ec1fb313c28814b8ad87b915
garbaczmaciej <furn20102@gmail.com>
2021-07-05 22:58:37 +0200

    Scraping works!

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from scraper_config import Config
 
 def is_int(string: str) -> bool:
     try:
-        int(string)
+        int(float(string))
         return True
     except ValueError:
         return False
@@ -18,37 +18,52 @@ class PdfScraper:
     def scrape_file(self) -> None:
 
         pdf_content = read_pdf(self.filename, pages="all", multiple_tables=True)
-        for table in pdf_content[16:]:
 
-            # Znajdź nazwę tabeli
-            table_name_row = table.head(0).keys()
-            for name in table_name_row:
-                if "Unnamed" not in name:
-                    break
+        for table_index, table in enumerate(pdf_content[1:]):
+
+            name = self.get_table_name(table)
 
             numbers_table = table.values.tolist()[1:]
-            results = dict()
-
-            a = False
-            for row in numbers_table:
-                if len(row) == 6:
-                    parsed_row = [(int(percent), int(percentile)) for percent, percentile in zip(row[::2], row[1::2]) if str(percent) != "nan"]
-                    for percent, percentile in parsed_row:
-                        results[percent] = percentile
-                else:
-                    print(row)
-                    a = True
-            if a:
-                print(numbers_table)
-                print()
-                print()
-                return
+            results = self.scrape_results(numbers_table)
 
             results_list = list(results.items())
             results_list.sort(key=lambda item: item[0])
 
             self.save_results_to_file(name, results_list)
             
+
+    def get_table_name(self, table) -> str:
+        table_name_row = table.head(0).keys()
+        for name in table_name_row:
+            if "Unnamed" not in name:
+                return name
+
+    def scrape_results(self, numbers_table) -> dict:
+        results = dict()
+
+        for row in numbers_table:
+            try:
+                parsed_row = [(int(percent), int(percentile)) for percent, percentile in zip(row[::2], row[1::2]) if str(percent) != "nan"]
+            except ValueError:
+                debugged_table = self.debug_numbers_table(numbers_table)
+                return self.scrape_results(debugged_table)
+            for percent, percentile in parsed_row:
+                results[percent] = percentile
+
+        return results
+
+    def debug_numbers_table(self, numbers_table) -> list:
+        debugged_table = list()
+        for row in numbers_table:
+            for item in row:
+                if is_int(item):
+                    debugged_table.append(str(int(float(item))))
+                elif type(item) == str and len(item.split()) > 1:
+                    for sub_item in item.split():
+                        if is_int(sub_item):
+                            debugged_table.append(str(int(float(sub_item))))
+                            
+        return [debugged_table]
 
 
     def save_results_to_file(self, name: str, results_list: list) -> None:
