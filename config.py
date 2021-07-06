f43f643fb96954108d7056c943fe5a35de47e662
garbaczmaciej <furn20102@gmail.com>
2021-07-06 17:04:29 +0200

    Added directory creation

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from os import mkdir, path
+
 class Config:
     # NAZWA PDFa Z TABELKAMI
     FILENAME = "centyle.pdf"
@@ -7,3 +9,9 @@ class Config:
 
     CHART_X = "Wynik"
     CHART_Y = "Centyl"
+
+if not path.exists(Config.DATA_DIR):
+    mkdir(Config.DATA_DIR)
+
+if not path.exists(Config.CHART_DIR):
+    mkdir(Config.DATA_DIR)
